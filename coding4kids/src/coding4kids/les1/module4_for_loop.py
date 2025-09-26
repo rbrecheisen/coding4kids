a3adb7bc70ecd5900506309e533f85f957e3afa8
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-09-26 07:51:39 +0200

    Renamed modules

---
+++ coding4kids/src/coding4kids/les1/module4_for_loop.py
@@ -0,0 +1,11 @@
+import time
+
+
+def main():
+    for i in range(5):
+        print('Hallo, wereld!')
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ coding4kids/src/coding4kids/les1/module5_if.py
@@ -0,0 +1,19 @@
+import time
+
+
+def main():
+    print_tekst = True
+    if print_tekst:
+        print('Hallo, wereld!')
+    else:
+        print('')
+
+    print_tekst = False
+    if print_tekst:
+        print('Hallo, wereld!')
+    else:
+        print('')
+
+
+if __name__ == '__main__':
+    main()
