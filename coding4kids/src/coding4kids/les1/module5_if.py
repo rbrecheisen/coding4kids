cca454ae1529b6ebc4a1803ef5abae4147a90826
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-09-26 07:55:15 +0200

    Saving work

--- coding4kids/src/coding4kids/les1/module5_if.py
+++ coding4kids/src/coding4kids/les1/module5_if.py
@@ -14,6 +14,23 @@ def main():
     else:
         print('')
 
+    hoogte = 10
+    if hoogte == 10:
+        print('Hoogte is 10')
+    else:
+        print('Hoogte is NIET 10')
+
+    if hoogte > 10:
+        print('Hoogte is groter dan 10')
+
+    if hoogte >= 10:
+        print('Hoogte is groter dan of gelijk aan 10')
+
+    if 3 < hoogte < 12:
+        print('Hoogte ligt tussen de 3 en 12')
+    else:
+        print('Hoogte is kleiner/gelijk aan 3 of groter/gelijk aan 12')
+
 
 if __name__ == '__main__':
     main()
