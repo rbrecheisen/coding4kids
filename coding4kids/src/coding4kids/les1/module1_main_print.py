5b5d0f3a29b3df1f317207e6b6d7a4cddaa6cede
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-09-27 08:54:30 +0200

    Saving work

--- coding4kids/src/coding4kids/les1/module1_main_print.py
+++ coding4kids/src/coding4kids/les1/module1_main_print.py
@@ -1,8 +1,15 @@
+def print_functie(waarde):
+    print(waarde)
+
+
 def main():
     print('Welkom bij Coding4Kids!')
     print(2 + 3)
     print(2 * 3)
     print(2 * (3 + 2))
+
+    print_functie('Welkom bij Coding4Kids!')
+    print_functie(2 * (3 + 2))
 
 
 if __name__ == '__main__':
