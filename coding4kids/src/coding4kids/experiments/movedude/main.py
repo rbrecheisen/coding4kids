b578341329e89e4e024cfdd12abab8f02048dfab
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-10-31 08:32:00 +0100

    Saving work

--- coding4kids/src/coding4kids/experiments/movedude/main.py
+++ coding4kids/src/coding4kids/experiments/movedude/main.py
@@ -1,18 +1,40 @@
 import tkinter as tk
 
+class Speler:
+    def __init__(self, canvas, x, y):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.image = tk.PhotoImage(file="dude_front.png")
+        self.icon = self.canvas.create_image(
+            self.x, self.y, image=self.image, anchor="nw")
+        self.width = self.image.width()
+        self.height = self.image.height()
+
+    def move(self, dx, dy):
+        self.canvas.move(self.icon, dx, dy)
+
+
 app = tk.Tk()
-app.title("Het speelveld")
+app.title("Bewegende speler")
 sky = tk.PhotoImage(file="sky.png")
-dude = tk.PhotoImage(file="dude_front.png")
 canvas = tk.Canvas(app, width=sky.width(), height=sky.height())
 canvas.create_image(0, 0, image=sky, anchor="nw")
-speler = canvas.create_image(50, 50, image=dude)
+speler = Speler(canvas, 50, 50)
 canvas.pack()
-
-def move_dude(event):
-    if event.keysym == 'Right':
-        canvas.move(speler, 10, 0)
+		
+def move_speler(event):
+    if event.keysym == "Right":
+        speler.move(10, 0)
+    elif event.keysym == "Left":
+        speler.move(-10, 0)
+    elif event.keysym == "Up":
+        speler.move(0, -10)
+    elif event.keysym == "Down":
+        speler.move(0, 10)
+    else:
+        print("Onbekende toets!")
 
 canvas.focus_set()
-canvas.bind('<KeyPress>', move_dude)
+canvas.bind("<KeyPress>", move_speler)
 app.mainloop()
